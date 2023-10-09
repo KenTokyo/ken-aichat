582abaa5da265395bcdccddeaf0980f5096cd773
KenTokyo <omed.hash@outlook.de>
2023-10-09 21:38:27 +0200

    new change

--- gpt-vercel/src/app/components/GPTLogo.tsx
+++ gpt-vercel/src/app/components/GPTLogo.tsx
@@ -1,4 +1,8 @@
-const GPTLogo = ({ imgName }) => {
+interface GPTLogoProps {
+  imgName: string;
+}
+
+const GPTLogo: React.FC<GPTLogoProps> = ({ imgName }) => {
   return (
     <div className="mr-4 ml-5">
       <img className="min-w-[40px] w-[40px]" src={imgName} alt="GPT LOGO" />
